c90b244fcdaac0191df16c9636777239f9d4c2dd
iklimov <iklimov@samokat.ru>
2025-02-24 19:40:42 +0000

    initial commit + packages and file structure

---
+++ src/app/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import {
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+function App(): React.JSX.Element {
+  return (
+      <View />
+  );
+}
+
+export default App;
